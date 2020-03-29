19f2e022439dcc03608f8205e78bae6db65372e3
liuyang <liuyang@coohua.com>
2020-03-29 16:17:44 +0800

    6

---
+++ DesignCode/Home.swift
@@ -0,0 +1,51 @@
+//
+//  Home.swift
+//  DesignCode
+//
+//  Created by liuyang on 2020/3/29.
+//  Copyright © 2020 liuyang. All rights reserved.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            MenuRow(image: "person.crop.circle", text: "My Account")
+            MenuRow(image: "creditcard", text: "Billing")
+            MenuRow(image: "person.and.person", text: "Team")
+            MenuRow(image: "arrow.uturn.down", text: "Sign out")
+            Spacer()
+        }
+        .padding(.top, 20)
+        .padding(30)
+        .frame(minWidth: .zero, maxWidth: .infinity)
+        .background(Color.white)
+        .cornerRadius(30)
+        .padding(.trailing, 60)
+        .shadow(radius: 20)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
+
+struct MenuRow: View {
+    var image = "creditcard"
+    var text = "My Account"
+    var body: some View {
+        HStack {
+            Image(systemName: image)
+                .imageScale(.large)
+                .foregroundColor(Color("icons"))
+                .frame(width: 32, height: 32)
+            
+            Text(text)
+                .font(.headline)
+            Spacer()
+        }
+    }
+}
