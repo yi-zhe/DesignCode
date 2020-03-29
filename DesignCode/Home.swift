5e8e08bd7617f6de4f53025fc026b9bed25310b2
liuyang <liuyang@coohua.com>
2020-03-29 16:30:44 +0800

    7-1

--- DesignCode/Home.swift
+++ DesignCode/Home.swift
@@ -9,12 +9,12 @@
 import SwiftUI
 
 struct Home: View {
+    var menu = menuData
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
-            MenuRow(image: "person.crop.circle", text: "My Account")
-            MenuRow(image: "creditcard", text: "Billing")
-            MenuRow(image: "person.and.person", text: "Team")
-            MenuRow(image: "arrow.uturn.down", text: "Sign out")
+            ForEach(menu) { item in
+                MenuRow(image: item.icon, text: item.title)
+            }
             Spacer()
         }
         .padding(.top, 20)
@@ -49,3 +49,20 @@ struct MenuRow: View {
         }
     }
 }
+
+/**
+ * 菜单数据模型
+ */
+struct Menu: Identifiable {
+    var id = UUID()
+    var title: String
+    var icon: String
+}
+
+let menuData = [
+    Menu(title: "My Account", icon: "person.crop.circle"),
+    Menu(title: "Billing", icon: "creditcard"),
+    Menu(title: "Team", icon: "person.and.person"),
+    Menu(title: "Sign out", icon: "arrow.uturn.down")
+]
+
